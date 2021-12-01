69ce670b346d858b93855ecc66c7e4b65f2816e2
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-12-01 13:54:28 -0600

    added seeds

---
+++ data/seeds/01-users.js
@@ -0,0 +1,20 @@
+
+exports.seed = function(knex) {
+  return knex('users').del()
+    .then(function () {
+      return knex('users').insert([
+        {
+          user_id: 1, 
+          username: 'coolhatguy', 
+          email: "fakeEmail@fake.com",
+          password: "password" // to be replaced with hash later
+        },
+        {
+          user_id: 2, 
+          username: 'bradford', 
+          email: "fakeEmail@fake.com",
+          password: "password2" // to be replaced with hash later
+        },
+      ]);
+    });
+};
---
+++ data/seeds/02-nicknames.js
@@ -0,0 +1,15 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('nicknames').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('nicknames').insert([
+        {nickname_id: 1, nickname: 'Beer Can Dan', user_id: 2},
+        {nickname_id: 2, nickname: 'Danny Diapers', user_id: 2},
+        {nickname_id: 3, nickname: '"Ass Man" Dan', user_id: 2},
+        {nickname_id: 4, nickname: 'Danny Daycare', user_id: 1},
+        {nickname_id: 5, nickname: '"Big Daddy" Dan', user_id: 1},
+      ]);
+    });
+};
