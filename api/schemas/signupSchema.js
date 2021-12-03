8c357aa8e5a3bbaee7947720c15dd2c464d89b23
Bradford-Meziere <coleokc@gmail.com>
2021-12-02 20:37:01 -0600

    Working addUser through GraphQL

--- api/schemas/signupSchema.js
+++ api/schemas/signupSchema.js
@@ -6,25 +6,46 @@ const {
 } = require("graphql");
 const bcrypt = require("bcryptjs");
 const user = require("../users/modal");
+const { isPlainObject } = require("lodash");
 
-const signupSchema = new GraphQLSchema({
-  name: "signupSchema",
+const userType = new GraphQLObjectType({
+  name: "user",
   fields: () => ({
-    addUser: {
-      name: "addUser",
-      type: GraphQLString,
-      args: {
-        email: { type: GraphQLString },
-        password: { type: GraphQLString },
-        username: { type: GraphQLString },
-      },
-      resolve: (parent, args) => {
-        const hash = bcrypt.hashSync(args.password, 10);
-        user.addUser({ ...args, password: hash }).then((res) => {
-          return res;
-        });
-      },
-    },
+    user_id: { type: GraphQLInt },
+    email: { type: GraphQLString },
+    password: { type: GraphQLString },
+    username: { type: GraphQLString },
   }),
 });
+const signupSchema = new GraphQLSchema({
+  name: "UserSchema",
+  query: new GraphQLObjectType({
+    name: "userQuery",
+    fields: () => ({
+      user: {
+        type: userType,
+      },
+    }),
+  }),
+  mutation: new GraphQLObjectType({
+    name: "userMutation",
+    fields: () => ({
+      addUser: {
+        name: "addUser",
+        type: userType,
+        args: {
+          email: { type: GraphQLString },
+          password: { type: GraphQLString },
+          username: { type: GraphQLString },
+        },
+        resolve: async (parent, args) => {
+          const hash = bcrypt.hashSync(args.password, 10);
+          const newUser = await user.addUser({ ...args, password: hash });
+          return newUser;
+        },
+      },
+    }),
+  }),
+});
+
 module.exports = signupSchema;
