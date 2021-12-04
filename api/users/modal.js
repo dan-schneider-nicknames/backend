3f577d404b1c46b9592a983d1f919cb01d2ed437
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-12-03 18:58:36 -0600

    dried modal code

--- api/users/modal.js
+++ api/users/modal.js
@@ -1,14 +1,18 @@
 const db = require("../../data/db-config")
+const { users } = require("../../data/tableNames")
 
-const addUser = (user) => {
-    return db("users").insert(user)
-    .then(([user_id])=>{
-        return db("users").where({user_id}).first()
-    });
+const addUser = user => {
+    return db(users)
+        .insert(user)
+        .then(([user_id])=>{
+            return getUserById(user_id)
+        });
 }
 
 const getUserById = user_id => {
-
+    return db(users)
+        .where({ user_id })
+        .first()
 }
 
 const verifyUser = () => {
