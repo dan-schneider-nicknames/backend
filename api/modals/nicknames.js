22e7e28fec0e3b73f5136ef732bffba60403f436
Bradford-Meziere <coleokc@gmail.com>
2021-12-14 18:50:23 -0600

    Asynchronous nicknameModal

--- api/modals/nicknames.js
+++ api/modals/nicknames.js
@@ -1,51 +1,64 @@
-const db = require("../../data/db-config")
-const { nicknames, likes } = require("../../data/tableNames")
+const db = require("../../data/db-config");
+const { nicknames, likes } = require("../../data/tableNames");
 
-const getUserNicknames = async user_id => {
-    try {
-        return await db(nicknames).where({ user_id })
-    } catch(err) {
-        console.log(err)
-    }
-}
+const getUserNicknames = async (user_id) => {
+  try {
+    return await db(nicknames).where({ user_id });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-const getNicknames = () => db(nicknames)
+const getNicknames = async () => {
+  try {
+    await db(nicknames);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-const getNicknameLikes = async nickname_id => {
-    try {
-        const { length } = await db(likes).where({ nickname_id })
-        return length
-    } catch(err) {
-        console.log(err)
-    }
-}
+const getNicknameLikes = async (nickname_id) => {
+  try {
+    const { length } = await db(likes).where({ nickname_id });
+    return length;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-const getNicknameById = nickname_id => {
-    return db(nicknames)
-    .where({ nickname_id })
-    .first()
-}
+const getNicknameById = async (nickname_id) => {
+  try {
+    const call = await db(nicknames).where({ nickname_id }).first();
+    return call;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-const addNickname = (nickname) => {
-    return db(nicknames)
-    .insert(nickname)
-    .then(([nickname_id]) => {
-        return getNicknameById(nickname_id)
-    })
-}
+const addNickname = async (nickname) => {
+  try {
+    const [nickname_id] = await db(nicknames).insert(nickname);
+    const newNick = await getNicknameById(nickname_id);
+    return newNick;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-const deleteNickname = nickname_id => {
-    return db(nicknames)
-    .where({ nickname_id })
-    .del()
-}
-
+const deleteNickname = async (nickname_id) => {
+  try {
+    const call = await db(nicknames).where({ nickname_id }).del();
+    return call;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = {
-    getUserNicknames,
-    getNicknameLikes,
-    getNicknameById,
-    addNickname,
-    deleteNickname,
-    getNicknames
-}
+  getUserNicknames,
+  getNicknameLikes,
+  getNicknameById,
+  addNickname,
+  deleteNickname,
+  getNicknames,
+};
